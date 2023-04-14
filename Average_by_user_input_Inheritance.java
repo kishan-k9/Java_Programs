498c7b11e7769cc51b5d997a4f0d34ffa2d2e3b6
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2023-04-14 21:17:11 +0530

    Add files via upload

---
+++ Average_by_user_input_Inheritance.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Par {
+    Scanner sc = new Scanner(System.in);
+    int n,i;
+    int a[];
+
+    void getd() {
+        System.out.println("How many numbers there are : ");
+        n = sc.nextInt();
+        a=new int[n];
+        System.out.println("Enter the numbers...");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+    }
+}
+
+class ch1 extends Par {
+    int sum=0;
+    float av;
+
+    void avg() {
+        for(i=0;i<n;i++){
+            sum=sum+a[i];
+        }
+        av=(float)sum/n;
+    }
+}
+
+class ch2 extends ch1 {
+    void show() {
+        System.out.println("The avg is : "+av);
+    }
+}
+
+public class Average_by_user_input_Inheritance {
+    public static void main(String[] args) {
+        ch2 o = new ch2();
+        o.getd();
+        o.avg();
+        o.show();
+    }
+}
