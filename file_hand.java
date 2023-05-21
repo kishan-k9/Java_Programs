26737d55ce0bdc2c7261f1f1dbf981c387545214
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2023-05-21 22:53:26 +0530

    file_hand.java

--- file_hand.java
+++ file_hand.java
@@ -1,59 +1,80 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 class file_hand {
-    public static void main(String[] args) throws IOException {
-        /*        
-          File myFile = new File("file_1.txt");     // creating a txt file
-          try {
-          boolean value = myFile.createNewFile();
-          if(value){
-          System.out.println("New file is created..");
-          }else{
-          System.out.println("the file is all ready exist");
-          }
-          } catch (IOException e) {
-          System.out.println("unable to create this file");
-          e.getStackTrace();
-          }
-         
-        try {    // writing to a file..
-            FileWriter filew = new FileWriter("file_1.txt");
-            filew.write("this is java cool\nbaby");
-            filew.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        // reading to a file
+    void Create() throws IOException{
+        File myFile = new File("file_1.txt");     
+            boolean value = myFile.createNewFile();
+            if(value){
+                System.out.println("New file is created..");
+            }else{
+                System.out.println("the file is all ready exist");
+            }
+    }
+    void Write() throws IOException{
+        FileWriter filew1 = new FileWriter("file_1.txt");
+        filew1.write("this is java cool");
+        filew1.close();
+    }
+    void wc() throws IOException{
+        Scanner s= new Scanner(System.in);  
+        System.out.print("Enter a String: ");  
+        String str= s.nextLine(); 
+		FileWriter fw=new FileWriter("file_1.txt");
+		for (int i = 0; i < str.length(); i++)
+			fw.write(str.charAt(i));
+
+		System.out.println("Writing successful");
+		fw.close();
+    }
+    void Read() throws IOException{
         File myFile = new File("file_1.txt");
-        try{
             Scanner sc = new Scanner(myFile);
             while(sc.hasNextLine()){
                 String Line=sc.nextLine();
                 System.out.println(Line);
             }
             sc.close();
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
-        
-        //deleting a file
-        File myFile = new File("New Text Document.txt");
+    }
+    void Delete() throws IOException{
+        File myFile = new File("file_1.txt");
         if(myFile.delete()){
             System.out.println("i have deleted : "+myFile.getName());
         }
         else{
             System.out.println("Some problem is error...");
         }
-        */
+    }
+    public static void main(String[] args) throws IOException {
+        file_hand obj = new file_hand();
+        Scanner x = new Scanner(System.in);
+        int n;
+        System.out.println("File Handling Operations");
+        do {
+            System.out.println("1. Create file\n2. Write into File\n3. Write from console\n4. Read from file\n5. Delete File\n6. Exit");
+            System.out.println("Choose option : ");
+            n=x.nextInt();
+            switch(n){
+                case 1:
+                    obj.Create();
+                break;
+                case 2:
+                    obj.Write();
+                break;
+                case 3:
+                    obj.wc();
+                    break;
+                case 4:
+                    obj.Read();
+                    break;
+                case 5:
+                    obj.Delete();  
+                break;
+                default:
+                    System.out.println("Good_Bye");
+                break;
+            }
+        } while (n!=6);
+        x.close();
     }
 }
-/*
- * You can see the file_1.txt in your saved java programs..
- * output
- * New file is created..
- * 
- */
